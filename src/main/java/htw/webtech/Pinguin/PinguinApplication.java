cf8611528e52d8f15ab4223d7af6a6e8fff006d0
Michelinchen <michael.kecker@googlemail.com>
2026-04-17 10:32:53 +0200

    Initial commit

---
+++ src/main/java/htw/webtech/Pinguin/PinguinApplication.java
@@ -0,0 +1,13 @@
+package htw.webtech.Pinguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinguinApplication.class, args);
+	}
+
+}
