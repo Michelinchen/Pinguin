b5db21c752bb3023a32c5b79a7fdc99de0b4b4a2
Michelinchen <michael.kecker@googlemail.com>
2026-04-17 11:11:51 +0200

    record Klasse

--- src/main/java/htw/webtech/Pinguin/Macronutrient.java
+++ src/main/java/htw/webtech/Pinguin/Macronutrient.java
@@ -1,25 +1,4 @@
 package htw.webtech.Pinguin;
 
-public class Macronutrient {
-    private  double countFat;
-    private  double countCarbs;
-    private  double countProteins;
-
-    public Macronutrient(double countFat, double countCarbs, double countProteins) {
-        this.countFat = countFat;
-        this.countCarbs = countCarbs;
-        this.countProteins = countProteins;
-    }
-
-    public double getCountFat() {
-        return countFat;
-    }
-
-    public double getCountCarbs() {
-        return countCarbs;
-    }
-
-    public double getCountProteins() {
-        return countProteins;
-    }
+public record Macronutrient(double countFat, double countCarbs, double countProteins) {
 }
